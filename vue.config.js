00366472312e07f0c4d97a59f7a2b179c6b241d7
Marco Sameh <mmikhail@tanmeyah.com>
2024-09-02 14:54:50 +0300

    updates

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Prescription-App/' // This should match the repository name
+    : '/'
 };
+
